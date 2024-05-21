0bb0fbf9a9ad2df39f5dc0c22f52b315df24ba42
StlZero <ketyanuk3chim1@gmail.com>
2024-05-21 20:44:42 +0700

    Added create todo list functionality

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import "./styles/index.css";
 
-function addTodo(title, description, dueDate, priority, notes) {
+function createTodo(title, description, dueDate, priority, notes) {
     this.title = title;
     this.description = description;
     this.dueDate = dueDate;
@@ -11,7 +11,7 @@ function addTodo(title, description, dueDate, priority, notes) {
 const todoLists = [];
 
 function pushTodo(title, description, dueDate, priority, notes) {
-    let newTodo = new addTodo("Brush teeth", "Get up and brush my teeth", "January 1st 2022", "High", "Use colgate");
+    let newTodo = new createTodo("Brush teeth", "Get up and brush my teeth", "January 1st 2022", "High", "Use colgate");
     // ("Brush teeth", "Get up and brush my teeth", "January 1st 2022", "High", "Use colgate")
     return todoLists.push(newTodo);
 }
@@ -22,6 +22,28 @@ function loadTodo() {
     const list_contents = document.querySelector(".list-contents");
     const newDiv = document.createElement('div');
     newDiv.innerHTML += `
-    
+    <div class="todo">
+        <div class="todo-1">
+            <p>Brush teeth</p>
+        </div>
+        <div class="todo-2">
+            <div>DETAILS</div>
+            <div>DATE</div>
+            <div>EDIT</div>
+            <div>DELETE</div>
+        </div>
     `;
+
+    list_contents.appendChild(newDiv);
 }
+
+
+function addTodo() {
+    const createButton = document.querySelector('.create');
+
+    createButton.addEventListener('click', (e) => {
+        loadTodo();
+    });
+}
+
+addTodo();
