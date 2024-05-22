51dc1f017b36b5491339c32232b909578023ee9a
StlZero <ketyanuk3chim1@gmail.com>
2024-05-22 16:03:05 +0700

    Added custom todo functionality

--- src/index.js
+++ src/index.js
@@ -19,16 +19,20 @@ function pushTodo(title, description, dueDate, priority, notes) {
 // pushTodo();
 
 function loadTodo() {
+    const title_input = document.getElementById('title');
+    const description_input = document.getElementById('description');
+    const due_date_input = document.getElementById('dueDate');
+
     const list_contents = document.querySelector(".list-contents");
     const newDiv = document.createElement('div');
     newDiv.innerHTML += `
     <div class="todo">
         <div class="todo-1">
-            <p>Brush teeth</p>
+            <p>${title_input.value}</p>
         </div>
         <div class="todo-2">
-            <div>DETAILS</div>
-            <div>DATE</div>
+            <div class="details" data-description="${description_input.value}">DETAILS</div>
+            <div>${due_date_input.value}</div>
             <div>EDIT</div>
             <div>DELETE</div>
         </div>
@@ -46,4 +50,73 @@ function addTodo() {
     });
 }
 
-addTodo();
+// addTodo();
+
+
+function createDialog() {
+    const content = document.querySelector('#content');
+    const createButton = document.querySelector('.create');
+
+    const dialog = document.createElement('dialog');
+    dialog.classList.add('todoDialog');
+    dialog.innerHTML += `
+    <p>Greetings, one and all!</p>
+    <form method="dialog">
+    <div>
+        <label for="title">Title</label>
+        <input type="text" name="title" id="title"/>
+    </div>
+
+    <div>
+        <label for="description">Description</label>
+        <input type="text" name="description" id="description" />
+    </div>
+
+    <div>
+        <label for="dueDate">Due date</label>
+        <input type="number" name="dueDate" id="dueDate" />
+    </div>
+
+    <button type="submit" id="submit-button">Submit</button>
+    </form>
+    `;
+    content.appendChild(dialog);
+
+    // 
+    // const title_input = document.getElementById('title');
+    // const description_input = document.getElementById('description');
+    // const due_date_input = document.getElementById('dueDate');
+
+    // 
+
+    createButton.addEventListener('click', (e) => {
+        dialog.showModal();
+    });
+
+    const closeButton = document.querySelector("dialog button");
+    closeButton.addEventListener('click', (e) => {
+        loadTodo();
+        dialog.close();
+    });
+}
+
+createDialog();
+
+function showDetails() {
+    const divElement = document.querySelector('div');
+    divElement.addEventListener('click', (e) => {
+        if(e.target.className === 'details') {
+            const dialog = document.createElement('dialog');
+            dialog.classList.add('rand-dialog');
+            dialog.innerHTML += `
+            <p>${e.target.dataset.description}</p>
+            <form method="dialog">
+            <button>OK</button>
+            </form>
+            `;
+            content.appendChild(dialog);
+            dialog.showModal();
+          }
+    });
+}
+showDetails();
