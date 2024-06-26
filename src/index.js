c69cc7345173f444eeaded273c6fdc5cc0ef5b0e
StlZero <ketyanuk3chim1@gmail.com>
2024-06-26 15:45:40 +0700

    More commit

--- src/index.js
+++ src/index.js
@@ -1,115 +1,152 @@
 import "./styles/index.css";
 
-function createTodo(title, description, dueDate, priority, notes) {
-    this.title = title;
-    this.description = description;
-    this.dueDate = dueDate;
-    this.priority = priority;
-    this.notes = notes;
-}
-
-const todoLists = [];
-
-function pushTodo(title, description, dueDate, priority, notes) {
-    let newTodo = new createTodo("Brush teeth", "Get up and brush my teeth", "January 1st 2022", "High", "Use colgate");
-    // ("Brush teeth", "Get up and brush my teeth", "January 1st 2022", "High", "Use colgate")
-    return todoLists.push(newTodo);
-}
-
-// pushTodo();
-
-function loadTodo() {
-    const title_input = document.getElementById('title');
-    const description_input = document.getElementById('description');
-    const due_date_input = document.getElementById('dueDate');
-
-    const list_contents = document.querySelector(".list-contents");
-    const newDiv = document.createElement('div');
-    newDiv.innerHTML += `
+function todo_add_HTML(div, title, desc, date) {
+    div.innerHTML = `
     <div class="todo">
-        <div class="todo-1">
-            <p>${title_input.value}</p>
-        </div>
-        <div class="todo-2">
-            <div class="details" data-description="${description_input.value}">DETAILS</div>
-            <div>${due_date_input.value}</div>
-            <div>EDIT</div>
-            <div>DELETE</div>
-        </div>
+    <div class="todo-1">
+        <p>${title}</p>
+    </div>
+    <div class="todo-2">
+        <div class="details" data-description="${desc}">DETAILS</div>
+        <div>${date}</div>
+        <div id="edit-div">EDIT</div>
+        <div id="delete-div">DELETE</div>
+    </div>
     `;
-
-    list_contents.appendChild(newDiv);
 }
 
 
-function addTodo() {
-    const createButton = document.querySelector('.create');
 
-    createButton.addEventListener('click', (e) => {
-        loadTodo();
+function dialog_add_HTML(dialog, title, desc, date) {
+    dialog.innerHTML = `
+    <p>Greetings, one and all!</p>
+    <form method="dialog" id="dialog-form">
+    <div>
+        <label for="title">Title</label>
+        <input type="text" name="title" id="title" value="${title}" required/>
+    </div>
+
+    <div>
+        <label for="description">Description</label>
+        <input type="text" name="description" id="description" value="${desc}"/>
+    </div>
+
+    <div>
+        <label for="dueDate">Due date</label>
+        <input type="number" name="dueDate" id="dueDate" value="${date}" required/>
+    </div>
+
+    <button type="submit" id="submit-button">Submit</button>
+    </form>
+
+    <button id="close-button">Close</button>
+    `;
+};
+
+
+
+function rand_add_HTML(dialog, desc) {
+    dialog.innerHTML = `
+    <p>${desc}</p>
+    <form method="dialog">
+    <button>OK</button>
+    </form>
+    `;
+}
+
+
+
+function todosApp(element, title, desc, date) {
+    // Create dialog
+    const content = document.querySelector('#content');
+    const dialog = document.createElement('dialog');
+    dialog.classList.add('todoDialog');
+    dialog_add_HTML(dialog, title, desc, date);
+    content.appendChild(dialog);
+
+    // Create description dialog
+    const descDialog = document.createElement('dialog');
+    descDialog.classList.add('rand-dialog');
+    rand_add_HTML(descDialog, desc);
+    content.appendChild(descDialog);
+
+    element.addEventListener('click', (e) => {
+        if (e.target.id == 'edit-div') {
+            dialog.showModal();
+        }
+
+        if (e.target.id == 'delete-div') {
+            dialog.remove();
+            descDialog.remove();
+            element.remove();
+        }
+        if (e.target.className == 'details') {
+            descDialog.showModal();
+        }
+    });
+
+    // Input values
+    const title_input = dialog.querySelector('#title');
+    const description_input = dialog.querySelector('#description');
+    const due_date_input = dialog.querySelector('#dueDate');
+    dialog.addEventListener('click', (e) => {
+        // Submit form
+        if (e.target.id == 'submit-button') {
+            // Form validation
+            if ((title_input.value != '') && (due_date_input.value != '')) {
+                todo_add_HTML(element, title_input.value, description_input.value, due_date_input.value);
+                rand_add_HTML(descDialog, description_input.value);
+
+                dialog.close();
+            }
+        }
+        // Close dialog
+        if (e.target.id == 'close-button') {
+            dialog.close();
+        }
     });
 }
 
-// addTodo();
 
 
 function createDialog() {
     const content = document.querySelector('#content');
     const createButton = document.querySelector('.create');
+    const list_contents = document.querySelector(".list-contents");
 
+    // Create dialog
     const dialog = document.createElement('dialog');
     dialog.classList.add('todoDialog');
-    dialog.innerHTML += `
-    <p>Greetings, one and all!</p>
-    <form method="dialog">
-    <div>
-        <label for="title">Title</label>
-        <input type="text" name="title" id="title"/>
-    </div>
-
-    <div>
-        <label for="description">Description</label>
-        <input type="text" name="description" id="description" />
-    </div>
-
-    <div>
-        <label for="dueDate">Due date</label>
-        <input type="number" name="dueDate" id="dueDate" />
-    </div>
-
-    <button type="submit" id="submit-button">Submit</button>
-    </form>
-    `;
+    dialog_add_HTML(dialog, '', '', '');
     content.appendChild(dialog);
-
-    createButton.addEventListener('click', (e) => {
+    createButton.addEventListener('click', () => {
         dialog.showModal();
     });
 
-    const closeButton = document.querySelector("dialog button");
-    closeButton.addEventListener('click', (e) => {
-        loadTodo();
-        dialog.close();
+    // Input values
+    const title_input = document.getElementById('title');
+    const description_input = document.getElementById('description');
+    const due_date_input = document.getElementById('dueDate');
+
+    dialog.addEventListener('click', (e) => {
+        // Submit form
+        if (e.target.id == 'submit-button') {
+            // Form validation
+            if ((title_input.value != '') && (due_date_input.value != '')) {
+                let newDiv = document.createElement('div');
+                todo_add_HTML(newDiv, title_input.value, description_input.value, due_date_input.value);
+                list_contents.appendChild(newDiv);
+
+                todosApp(newDiv, title_input.value, description_input.value, due_date_input.value);
+
+                dialog.close();
+                e.target.parentElement.reset();
+            }
+        }
+        // Close dialog
+        if (e.target.id == 'close-button') {
+            dialog.close();
+        }
     });
 }
-
 createDialog();
-
-function showDetails() {
-    const divElement = document.querySelector('div');
-    divElement.addEventListener('click', (e) => {
-        if(e.target.className === 'details') {
-            const dialog = document.createElement('dialog');
-            dialog.classList.add('rand-dialog');
-            dialog.innerHTML += `
-            <p>${e.target.dataset.description}</p>
-            <form method="dialog">
-            <button>OK</button>
-            </form>
-            `;
-            content.appendChild(dialog);
-            dialog.showModal();
-          }
-    });
-}
-showDetails();
